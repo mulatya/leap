8bd3674f98e56e8bb655825f8a760e9c84038f3b
mulatya <emmanuelmulatya8@gmail.com>
2017-06-07 12:11:48 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var leapYear = function(year){
+  result false;
+};
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+    });
+  });
